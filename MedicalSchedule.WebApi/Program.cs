43ffa18dd83ed38033a6ca6d70d278454f7ad058
Thiago H <thiagodiacho@gmail.com>
2026-06-03 00:44:34 -0300

    Implements CORS policy to allow flutter requests

--- MedicalSchedule.WebApi/Program.cs
+++ MedicalSchedule.WebApi/Program.cs
@@ -23,6 +23,15 @@ builder.Services.AddProblemDetails();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFlutter",
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -34,6 +43,7 @@ if (app.Environment.IsDevelopment())
 app.UseExceptionHandler();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseCors("AllowFlutter");
 app.MapControllers();
 
 app.Run();
