743116a427fc2284fa84ad1717ef64601a02d756
Thiago H <thiagodiacho@gmail.com>
2026-06-03 20:09:55 -0300

    Implements app_router to use the routes defined in the config

--- MedicalSchedule.Frontend/medical_schedule_app/lib/core/routes/app_router.dart
+++ MedicalSchedule.Frontend/medical_schedule_app/lib/core/routes/app_router.dart
@@ -1,16 +1,18 @@
 import 'package:go_router/go_router.dart';
 
+import '../config/routes.dart';
+
 import '../../pages/auth/login_screen.dart';
 import '../../pages/auth/signup_screen.dart';
 
 final GoRouter appRouter = GoRouter(
   routes: [
     GoRoute(
-      path: '/',
+      path: Routes.login,
       builder: (_, _) => const LoginScreen(),
     ),
     GoRoute(
-      path: '/signup',
+      path: Routes.signup,
       builder: (_, _) => const SignupScreen(),
     ),
   ],
