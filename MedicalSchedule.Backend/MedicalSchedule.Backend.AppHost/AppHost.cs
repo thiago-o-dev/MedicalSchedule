9338380853924f7e0167c63d201d99b382520fbb
Thiago H <thiagodiacho@gmail.com>
2026-06-05 03:21:31 -0300

    Orchestrated the full application, all fine on the aspire lifeline

--- MedicalSchedule.Backend/MedicalSchedule.Backend.AppHost/AppHost.cs
+++ MedicalSchedule.Backend/MedicalSchedule.Backend.AppHost/AppHost.cs
@@ -1,3 +1,64 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgresPassword = builder.AddParameter("postgres-password", "keycloak", secret: true);
+var keycloakPassword = builder.AddParameter("keycloak-password", "thiagozika", secret: true);
+
+// Databases
+
+var postgres = builder.AddPostgres("postgres", password: postgresPassword)
+    .WithDataVolume();
+
+var keycloakDb = postgres.AddDatabase("keycloak-db");
+
+var schedulingDb = postgres.AddDatabase("scheduling-db");
+
+var paymentsDb = postgres.AddDatabase("payments-db");
+
+var notificationsDb = postgres.AddDatabase("notifications-db");
+
+// Infrastructure
+
+var redis = builder.AddRedis("redis");
+
+var rabbitmq = builder.AddRabbitMQ("rabbitmq");
+
+var keycloak = builder.AddKeycloak("keycloak", 8081)
+    .WithReference(keycloakDb)
+    .WithEnvironment("KEYCLOAK_ADMIN", "pedropedro")
+    .WithEnvironment("KEYCLOAK_ADMIN_PASSWORD", keycloakPassword)
+    .WaitFor(postgres);
+
+
+
+// Services
+
+var scheduling =
+    builder.AddProject<Projects.Scheduling>("scheduling")
+        .WithReference(redis)
+        .WithReference(rabbitmq)
+        .WithReference(schedulingDb);
+
+var payments =
+    builder.AddProject<Projects.Payments>("payments")
+        .WithReference(redis)
+        .WithReference(rabbitmq)
+        .WithReference(paymentsDb);
+
+var notifications =
+    builder.AddProject<Projects.Notifications>("notifications")
+        .WithReference(rabbitmq)
+        .WithReference(notificationsDb);
+
+var whatsapp =
+    builder.AddProject<Projects.WhatsappBot>("whatsappbot")
+        .WithReference(redis)
+        .WithReference(rabbitmq);
+
+var gateway =
+    builder.AddProject<Projects.Api_Gateway>("gateway")
+        .WithReference(scheduling)
+        .WithReference(payments)
+        .WithReference(notifications)
+        .WithReference(whatsapp);
+
 builder.Build().Run();
--- MedicalSchedule.Backend/Services/Notifications/Program.cs
+++ MedicalSchedule.Backend/Services/Notifications/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.AddServiceDefaults();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -7,6 +9,8 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
