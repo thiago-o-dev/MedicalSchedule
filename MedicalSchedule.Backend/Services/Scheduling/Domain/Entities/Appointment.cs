3b7e3a56531c6024a19d96e76af746a0482e967e
Thiago H <thiagodiacho@gmail.com>
2026-06-05 15:50:03 -0300

    Creates Registry service

---
+++ MedicalSchedule.Backend/Services/Scheduling/Domain/Entities/Appointment.cs
@@ -0,0 +1,16 @@
+﻿namespace Scheduling.Domain.Entities;
+
+public class Appointment
+{
+    // Maybe this on the future would be nice
+    //IEnumerable<Guid> VetIds;
+    //IEnumerable<Guid> PetIds;
+    //IEnumerable<Guid> OwnerIds;
+
+    Guid VetId;
+    Guid PetId;
+    Guid OwnerId;
+
+    DateTime Date;
+    TimeSpan EstimatedDuration;
+}
